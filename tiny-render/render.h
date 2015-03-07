85fa284e55bdb21c695c4490458cf2b966ad3216
misko.pawel <misko.pawel@gmail.com>
2015-03-08 00:46:48 +0300

    removed wireframe rendering

--- tiny-render/render.h
+++ tiny-render/render.h
@@ -7,6 +7,5 @@ namespace sharpeye
 	namespace gil = boost::gil;
 
 	void render_model( gil::rgb8_view_t const & view, Model const & model );
-	void draw_wireframe( gil::rgb8_view_t const & view, Model const & model, gil::rgb8_pixel_t const & color );
 
 } // ns sharpeye
